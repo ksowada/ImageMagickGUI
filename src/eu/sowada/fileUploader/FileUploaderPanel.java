4830c7fb92e7b16cb3b41b701c9f3215506d4e63
Karl S. <gokaertle@gmx.de>
2019-01-13 16:04:42 +0100

    fill list with multiple selcted files

--- src/eu/sowada/fileUploader/FileUploaderPanel.java
+++ src/eu/sowada/fileUploader/FileUploaderPanel.java
@@ -8,7 +8,6 @@ import java.awt.event.KeyEvent;
 import java.io.File;
 
 import javax.swing.ImageIcon;
-import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
@@ -25,9 +24,8 @@ public class FileUploaderPanel extends JPanel implements ActionListener {
 
 	private static final long serialVersionUID = 1042945819029455828L;
 	static private final String newline = "\n";
-	JButton openButton, saveButton;
 	JTextArea log;
-	JFileChooser fc;
+	JFileChooser fileChooser;
 	
     JMenuBar menuBar;
     JMenu menuFile;
@@ -44,35 +42,9 @@ public class FileUploaderPanel extends JPanel implements ActionListener {
 		JScrollPane logScrollPane = new JScrollPane(log);
 
 //Create a file chooser
-		fc = new JFileChooser();
+		fileChooser = new JFileChooser();
+		fileChooser.setMultiSelectionEnabled(true);
 
-//Uncomment one of the following lines to try a different
-//file selection mode.  The first allows just directories
-//to be selected (and, at least in the Java look and feel,
-//shown).  The second allows both files and directories
-//to be selected.  If you leave these lines commented out,
-//then the default mode (FILES_ONLY) will be used.
-//
-//fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-//fc.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
-
-//Create the open button.  We use the image from the JLF
-//Graphics Repository (but we extracted it from the jar).
-		openButton = new JButton("Open a File...", createImageIcon("images/Open16.gif"));
-		openButton.addActionListener(this);
-
-//Create the save button.  We use the image from the JLF
-//Graphics Repository (but we extracted it from the jar).
-		saveButton = new JButton("Save a File...", createImageIcon("images/Save16.gif"));
-		saveButton.addActionListener(this);
-
-//For layout purposes, put the buttons in a separate panel
-		JPanel buttonPanel = new JPanel(); // use FlowLayout
-		buttonPanel.add(openButton);
-		buttonPanel.add(saveButton);
-
-//Add the buttons and the log to this panel.
-		add(buttonPanel, BorderLayout.PAGE_START);
 		add(logScrollPane, BorderLayout.CENTER);
 	}
 
@@ -102,45 +74,22 @@ public class FileUploaderPanel extends JPanel implements ActionListener {
 
 	public void actionPerformed(ActionEvent e) {
         if (e.getSource() == menuItemFileOpen) {
-            int returnVal = fc.showOpenDialog(FileUploaderPanel.this); //fileUploader.g());//.getComponent(0));//contentPane); //frame.getComponent(0));//.getContentPane());
+            int returnVal = fileChooser.showOpenDialog(FileUploaderPanel.this); //fileUploader.g());//.getComponent(0));//contentPane); //frame.getComponent(0));//.getContentPane());
             
-//            if (returnVal == JFileChooser.APPROVE_OPTION) {
-//                File file = fc.getSelectedFile();
-//                //This is where a real application would open the file.
-//                log.append("Opening: " + file.getName() + "." + newline);
-//            } else {
-//                log.append("Open command cancelled by user." + newline);
-//            }
-//            log.setCaretPosition(log.getDocument().getLength());
+            if (returnVal == JFileChooser.APPROVE_OPTION) {
+                File[] files = fileChooser.getSelectedFiles();
+
+                for (File file : files) {
+                //This is where a real application would open the file.
+                log.append("" + file.getName() + "" + newline);
+                }
+            } else {
+                log.append("Open command cancelled by user." + newline);
+            }
+            log.setCaretPosition(log.getDocument().getLength());
 
             System.out.println("something clicked in menuItemFileOpen");
         } 
-        
-//Handle open button action.
-		if (e.getSource() == openButton) {
-			int returnVal = fc.showOpenDialog(FileUploaderPanel.this);
-
-			if (returnVal == JFileChooser.APPROVE_OPTION) {
-				File file = fc.getSelectedFile();
-//This is where a real application would open the file.
-				log.append("Opening: " + file.getName() + "." + newline);
-			} else {
-				log.append("Open command cancelled by user." + newline);
-			}
-			log.setCaretPosition(log.getDocument().getLength());
-
-//Handle save button action.
-		} else if (e.getSource() == saveButton) {
-			int returnVal = fc.showSaveDialog(FileUploaderPanel.this);
-			if (returnVal == JFileChooser.APPROVE_OPTION) {
-				File file = fc.getSelectedFile();
-//This is where a real application would save the file.
-				log.append("Saving: " + file.getName() + "." + newline);
-			} else {
-				log.append("Save command cancelled by user." + newline);
-			}
-			log.setCaretPosition(log.getDocument().getLength());
-		}
 	}
 
 	/** Returns an ImageIcon, or null if the path was invalid. */
